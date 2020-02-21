4e5e2f8c61d5227eb8ff13c5e0ac6f291195c0e5
xml <xia_mengliang@163.com>
2020-02-21 17:40:30 +0800

    独立出初始化部分，完善文档，文档使用asciidoc

--- main.go
+++ main.go
@@ -31,6 +31,7 @@ type WallhavenConfig struct {
 }
 
 var cmdch chan int
+var cfg Config
 
 const (
 	version string = "1.1.0"
@@ -41,16 +42,18 @@ func getVersion() string {
 	return version
 }
 
-//export running
-func running(configpath string) {
+//export libInit
+func libInit(configpath string) {
 	cmdch = make(chan int, 0)
 	fmt.Println("core lib version:", version)
-	var cfg Config
 	_, err := toml.DecodeFile(configpath, &cfg)
 	if err != nil {
 		fmt.Println("err:", err)
 	}
+}
 
+//export running
+func running() {
 	for {
 		Handler(cfg)
 		select {
@@ -68,5 +71,6 @@ func next() {
 }
 
 func main() {
-	running(utils.GetCurrentDirectory() + "/config.toml")
+	libInit(utils.GetCurrentDirectory() + "/config.toml")
+	running()
 }
