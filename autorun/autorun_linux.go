8a16faa29f1c240912719b47be727c36554c9fa4
xml <xia_mengliang@163.com>
2020-02-25 09:22:55 +0800

    调制linux autorun

--- autorun/autorun_linux.go
+++ autorun/autorun_linux.go
@@ -1,7 +1,40 @@
 package autorun
 
+import (
+	"os"
+)
+
+func genService(fileName, serviceName string) error {
+	content := `[Unit]
+	Description=Wallpaper
+	
+	[Service]
+	` + fileName + `
+	
+	[Install]
+	WantedBy=default.target`
+
+	_, err := os.Stat(serviceName)
+	if err != nil {
+		//文件异常
+		var fd *os.File
+		fd, err = os.OpenFile(serviceName, 0, 0644)
+		if err != nil {
+			return err
+		}
+
+		defer fd.Close()
+
+		_, err = fd.WriteString(content)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func Enable(yes bool) bool {
-	return false
+	return true
 }
 
 func IsEnable() bool {
