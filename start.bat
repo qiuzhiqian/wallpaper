837c74e4e5ae0e06f8b1b86d05e99f8293fa1b16
xiamengliang <xiamengliang@uniontech.com>
2021-02-13 13:33:51 +0800

    移除无用文件，修复图标命名不一致问题

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set currentPath=%~dp0
-set tempPath=%currentPath%start.ps1
-
-set powershellcmd="& {set-executionpolicy Remotesigned -Scope Process; %appPath% }"
-echo %powershellcmd%
-
-powershell.exe -command ^ %powershellcmd%
